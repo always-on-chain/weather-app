206da262c387ef0edef925535c01b916c2dd8769
waynekwest88@gmail.com <waynekwest88@gmail.com>
2018-07-16 19:54:20 -0700

    connected to googleapis and have access to location information

---
+++ app.js
@@ -0,0 +1,10 @@
+const request = require('request');
+
+request({
+  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=5080%20liniki%20st%20honolulu%20hawaii',
+  json: true
+}, (error, response, body) => {
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Longitude: ${body.results[0].geometry.location.lat}`);
+})
