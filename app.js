03fdd480d6aea8c19e84420edcfbeba015fff188
waynekwest88@gmail.com <waynekwest88@gmail.com>
2018-07-18 19:00:20 -0700

    added error handling for api calls

--- app.js
+++ app.js
@@ -14,13 +14,19 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-let encodedAddress = encodeURIComponent(argv.a)
+let encodedAddress = encodeURIComponent(argv.address)
 
 request({
   url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
-  console.log(`Address: ${body.results[0].formatted_address}`);
-  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-  console.log(`Longitude: ${body.results[0].geometry.location.lat}`);
+  if (error) {
+    console.log('Unable to connect to Google servers.')
+  } else if (body.status === 'ZERO_RESULTS') {
+    console.log('Unable to find that address.');
+  } else if (body.status === 'OK') {
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lat}`);
+  }
 })
